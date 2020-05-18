3e141ab40e6053b225f846426520b98ccf3793e2
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-18 22:36:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,9 +4,15 @@
 #   "Amanda" => 27
 # }
 
+# def happy_birthday(birthday_kids)
+#   birthday_kids.each do | kids_name, age |
+#     puts "Happy Birthday #{kids_name}!
+#     You are now #{age} years old!"
+#   end
+# end
+
 def happy_birthday(birthday_kids)
-  birthday_kids.each do | kids_name, age |
-    puts "Happy Birthday #{kids_name}!
-    You are now #{age} years old!"
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
